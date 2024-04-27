d3975fca8873248237262974951cb4dd6d81425c
Luis <lundoesc@gmail.com>
2024-04-26 22:21:43 -0600

    simplifying eda code

--- custom_code/eda.py
+++ custom_code/eda.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from IPython.display import display
 
-def plot_default_rate(df, target_column, record_date_column, figsize, df_str=None, df_contrast=None, df_contrast_str=None):
-	
+def default_rate_summary(df, target_column, record_date_column):
 	df_default_rate_summary = (
 		df
 		.groupby(record_date_column)
@@ -14,26 +13,26 @@ def plot_default_rate(df, target_column, record_date_column, figsize, df_str=Non
 		).reset_index()
 	)
 
-	if df_contrast is not None:
-		df_contrast_default_rate_summary = (
-			df_contrast
-			.groupby(record_date_column)
-			.agg(default_rate = (target_column, "mean"))
-		)
-	else:
-		df_contrast_default_rate_summary = None
-
 	date = df_default_rate_summary[record_date_column].values
 	default_rate = df_default_rate_summary["default_rate"].values
 	counting = df_default_rate_summary["counting"].values
+	
+	return date, default_rate, counting, df_default_rate_summary
+
+def plot_default_rate(df, target_column, record_date_column, figsize, df_str=None, df_contrast=None, df_contrast_str=None):
+	
+	date, default_rate, counting, df_default_rate_summary = default_rate_summary(df, target_column, record_date_column)
+
+	if df_contrast is not None:
+		date_contrast, default_rate_contrast, _, _ = default_rate_summary(df_contrast, target_column, record_date_column)
 	
 	fig, ax0 = plt.subplots(figsize=figsize)
 	ax0.plot(date, default_rate, label=df_str)
 	ax0.tick_params(axis='x', rotation=90)
 	ax0.set(title="Default rate", xlabel = "Cohort", ylabel="Probability of default")
 
-	if df_contrast_default_rate_summary is not None:
-		ax0.plot(df_contrast_default_rate_summary, label=df_contrast_str, ls="--")
+	if df_contrast is not None:
+		ax0.plot(date_contrast, default_rate_contrast, label=df_contrast_str, ls="--")
 
 	ax0.legend()
 	
