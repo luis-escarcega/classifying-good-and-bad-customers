d4b05abb6ee54b46fbdf1f17577d55a40495165c
Luis <lundoesc@gmail.com>
2024-05-02 20:57:18 -0600

    adding stability test

--- custom_code/feature_selection.py
+++ custom_code/feature_selection.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score
+from optbinning import OptimalBinning
 from statsmodels.stats.proportion import proportions_ztest
+from sklearn.base import BaseEstimator, TransformerMixin
 from scipy.stats import chi2_contingency
 
 def _is_null_info_meaningful(y, predictor, significance):
@@ -27,16 +29,16 @@ def _null_info_study(X, y, significance):
 	    pval, test = _is_null_info_meaningful(y, predictor, significance)
 	    null_info_study.append((column, pval, test))
 
-	df_null_info_study = pd.DataFrame(null_info_study, columns=["predictor", "p_value", "is_null_info_meaningful"])
+	df_null_info_study = pd.DataFrame(null_info_study, columns=["predictor", "p_value_ztest", "is_null_info_meaningful"])
 	return df_null_info_study
 
 def null_info_study(X, y, significance=0.05, figsize=(10,7), top=None):
 	
 	df_null_info_study_orig = _null_info_study(X, y, significance)
-	df_null_info_study = df_null_info_study_orig.sort_values("p_value")
+	df_null_info_study = df_null_info_study_orig.sort_values("p_value_ztest")
 	
 	x = df_null_info_study["predictor"][:top]
-	y = df_null_info_study["p_value"][:top]
+	y = df_null_info_study["p_value_ztest"][:top]
 
 	fig, ax = plt.subplots(figsize=figsize)
 	ax.bar(x, y)
@@ -92,10 +94,76 @@ def gini_coefficient_bootstrapping(X, y, n_resamples=999, confidence_level=0.95,
 	
 	return df_ginis
 
-def test_of_homogeneity(data1, data2):
-	table = np.array([data1, data2])
-	res = chi2_contingency(table)
-	return res.statistic, res.pvalue
+class OptbinTransformer(BaseEstimator, TransformerMixin):
+    
+    def __init__(self, cols_drop, cols_null_indicator, cols_map_and_sense):
+        self.cols_drop = cols_drop
+        self.cols_null_indicator = cols_null_indicator
+        self.cols_map_and_sense = cols_map_and_sense
+    
+    def fit(self, X, y):
+        dict_of_optbins = {}
+        
+        for column, sense in self.cols_map_and_sense: 
+            predictor = X[column].values
+            optb = OptimalBinning(name=column, dtype="numerical", solver="cp", monotonic_trend=sense)
+            optb.fit(predictor, y)
+            dict_of_optbins[column] = optb
+        
+        self.dict_of_optbins = dict_of_optbins
+
+        return self
+
+    def transform(self, X, y=None):
+        # dropping columns that are not needed
+        X_transformed = X.drop(columns=self.cols_drop).copy()
+        # taking null indicator variables
+        X_transformed[self.cols_null_indicator] = X_transformed[self.cols_null_indicator].isna().astype(int)
+        # transforming the rest of columns
+        
+        for column, sense in self.cols_map_and_sense:
+            optb = self.dict_of_optbins[column]
+            X_transformed[column] = optb.transform(X_transformed[column], metric="event_rate", metric_missing="empirical")
+        
+        return X_transformed
+
+def test_stability(df, target_column, record_date_column, predictors_and_sense, significance=0.05):
+
+	stability_results = []
+	
+	record_date = df[record_date_column]
+	y = df[target_column]
+	
+	for column, sense in predictors_and_sense:
+		predictor = df[column]
+
+		if sense is not None:
+			optb = OptimalBinning(name=column, dtype="numerical", solver="cp", monotonic_trend=sense, max_n_prebins=4)
+			optb.fit(predictor, y)
+			predictor_indices = optb.transform(predictor, metric="indices")
+		else:
+			predictor_indices = predictor # it the null indicator variable
+		
+		df_predictor_indices = pd.DataFrame({
+			record_date_column : record_date, 
+			column : predictor_indices, 
+			target_column : y})
+		
+		df_predictor_indices[f"{column}-{target_column}"] = \
+			df_predictor_indices[column].astype(str) + df_predictor_indices[target_column].astype(str)
+
+		df_predictor_groupped = df_predictor_indices.groupby([record_date_column, f"{column}-{target_column}"]).size().reset_index()
+		df_predictor_pivoted = df_predictor_groupped.pivot(index=record_date_column, columns=f"{column}-{target_column}", values=0)
+		df_predictor_pivoted.fillna(0, inplace=True)
+		
+		contigency_table = df_predictor_pivoted.values
+		res = chi2_contingency(contigency_table)
+		
+		stability_results.append((column, res.statistic, res.pvalue, res.pvalue < significance))
+
+	df_stability_results = pd.DataFrame(stability_results, columns=["predictor", "chi_square_statistic", "p_value_chi_square", "is_unstable_variable"])
+	
+	return df_stability_results
 
 
 
